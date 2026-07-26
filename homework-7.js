03dbcc212730741c98d25239930409f973d10734
IBRAGIMOVGADJI <ddeerrbbeenntt@gmail.com>
2026-07-26 17:12:28 +0300

    homework-7, fixit error

--- homework-7.js
+++ homework-7.js
@@ -5,12 +5,12 @@ logTempCity("Дербент", 30);
 
 const SPEEDLIGHT = 299792458;
 function checkSpeed(speed) {
- if (speed > SPEEDLIGHT) {
-     console.log(`Скорость ${speed} м/с — Сверхсветовая скорость`);
- } else if (speed < SPEEDLIGHT) {
+  if (speed > SPEEDLIGHT) {
+    console.log(`Скорость ${speed} м/с — Сверхсветовая скорость`);
+  } else if (speed < SPEEDLIGHT) {
     console.log(`Скорость ${speed} м/с — Субсветовая скорость`);
- } else {
-     console.log(`Скорость ${speed} м/с — это Скорость света`);
+  } else {
+    console.log(`Скорость ${speed} м/с — это Скорость света`);
   }
 }
 checkSpeed(200000000);
@@ -18,11 +18,11 @@ checkSpeed(200000000);
 let product = 'RobotCleaner';
 let price = 10000;
 function buyProduct(budget) {
- if (budget >= price) {
+  if (budget >= price) {
   console.log('Робот-пылесос приобретён. Спасибо за покупку!');
-} else {
-let shortage = price - budget;
-  console.log(`Вам не хватает ${shortage} рублей, пополните баланс`);
+  } else {
+const SHORTAGE = price - budget;
+  console.log(`Вам не хватает ${SHORTAGE} рублей, пополните баланс`);
   }
 }
 buyProduct(3000);
